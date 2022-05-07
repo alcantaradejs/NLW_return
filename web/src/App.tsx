a31216c1e12c0d5bf03577ae79342c0f147e3799
BatatinhasFritas <alcantara.isaque167@gmail.com>
2022-05-07 13:35:13 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import Widget from "./components/widget"
+
+function App() {
+
+  return (
+    <Widget/>
+  )
+}
+
+export default App
---
+++ web/src/components/widget.tsx
@@ -0,0 +1,21 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+import { Popover } from '@headlessui/react'
+
+export default function Widget() {
+
+
+    return (
+        <Popover className="absolute bottom-4 right-4">
+            <Popover.Panel>hello</Popover.Panel>
+
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6" weight="regular" color="#fff"/>
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500">
+                    <span className="pl-2"></span>
+                    Feedback
+                </span>
+            </Popover.Button>
+        </Popover>
+        );
+}
